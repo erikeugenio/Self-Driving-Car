a95c9942896de9893e6c62aa530509421a206f81
erikeugenio <erik.eugenio@outlook.com>
2022-10-16 16:56:13 -0300

    Update

--- car.js
+++ car.js
@@ -1,80 +1,135 @@
-class Car{
-    constructor(x,y,width,height){
-        this.x=x;
-        this.y=y;
-        this.width=width;
-        this.height=height;
+class Car {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
 
-        this.speed=0;
-        this.acceleration=0.2;
-        this.maxSpeed=3;
-        this.friction=0.05;
-        this.angle=0;
+        this.speed = 0;
+        this.acceleration = 0.2;
+        this.maxSpeed = 3;
+        this.friction = 0.05;
+        this.angle = 0;
+        this.damaged=false;
 
         this.sensor = new Sensor(this);
         this.controls = new Controls();
-
+        //test
     }
 
-    update(){
-        this.#move();
-        this.sensor.update();
+    update(roadBoarders) {
+        if(!this.damaged){
+            this.#move();
+            this.polygon = this.#createPolygon();
+            this.damaged=this.#assessDamage(roadBoarders);
+        }
+        this.sensor.update(roadBoarders);
     }
 
-    #move(){
-        if(this.controls.forward){
-            this.speed+=this.acceleration;
-        }
-        if(this.controls.reverse){
-            this.speed-=this.acceleration;
-        }
-
-        if(this.speed>this.maxSpeed){
-            this.speed=this.maxSpeed;
-        }
-        if(this.speed<-this.maxSpeed/2){
-            this.speed=-this.maxSpeed/2;
-        }
-
-        if(this.speed>0){
-            this.speed-=this.friction;
-        }
-        if(this.speed<0){
-            this.speed+=this.friction;
-        }
-        if(Math.abs(this.speed)<this.friction){
-            this.speed=0;
-        }
-        if(this.speed!=0){
-            const flip=this.speed>0?1:-1;
-            if(this.controls.left){
-                this.angle+=0.03*flip;
-            }
-            if(this.controls.right){
-                this.angle-=0.03*flip;
+    #assessDamage(roadBoarders){
+        for(let i=0;i<roadBoarders.length;i++){
+            if(polysIntersect(this.polygon,roadBoarders[i])){
+                return true;
             }
         }
-
-        this.x-=Math.sin(this.angle)*this.speed;
-        this.y-=Math.cos(this.angle)*this.speed;
     }
 
-    draw(ctx){
-        ctx.save();
-        ctx.translate(this.x, this.y);
-        ctx.rotate(-this.angle);
+    #createPolygon() {
+        const points = [];
+        const rad = Math.hypot(this.width, this.height) / 2;
+        const alpha = Math.atan2(this.width, this.height);
+        points.push({
+            x: this.x - Math.sin(this.angle - alpha) * rad,
+            y: this.y - Math.cos(this.angle - alpha) * rad
+        });
+        points.push({
+            x: this.x - Math.sin(this.angle + alpha) * rad,
+            y: this.y - Math.cos(this.angle + alpha) * rad
+        });
+        points.push({
+            x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
+            y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad
+        });
+        points.push({
+            x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
+            y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad
+        });
+        return points;
+    }
 
 
+    #move() {
+        if (this.controls.forward) {
+            this.speed += this.acceleration;
+        }
+        if (this.controls.reverse) {
+            this.speed -= this.acceleration;
+        }
+
+        if (this.speed > this.maxSpeed) {
+            this.speed = this.maxSpeed;
+        }
+        if (this.speed < -this.maxSpeed / 2) {
+            this.speed = -this.maxSpeed / 2;
+        }
+
+        if (this.speed > 0) {
+            this.speed -= this.friction;
+        }
+        if (this.speed < 0) {
+            this.speed += this.friction;
+        }
+        if (Math.abs(this.speed) < this.friction) {
+            this.speed = 0;
+        }
+        if (this.speed != 0) {
+            const flip = this.speed > 0 ? 1 : -1;
+            if (this.controls.left) {
+                this.angle += 0.03 * flip;
+            }
+            if (this.controls.right) {
+                this.angle -= 0.03 * flip;
+            }
+        }
+
+        this.x -= Math.sin(this.angle) * this.speed;
+        this.y -= Math.cos(this.angle) * this.speed;
+    }
+
+    draw(ctx) {
+
+        if(this.damaged){
+            ctx.fillStyle="gray";
+        }else{
+            ctx.fillStyle="black";
+        }
+
+        // Antes do uso da função de poligono
+        /*        
+                ctx.save();
+                ctx.translate(this.x, this.y);
+                ctx.rotate(-this.angle);
+        
+        
+                ctx.beginPath();
+                ctx.rect(
+                    -this.width / 2,
+                    -this.height / 2,
+                    this.width,
+                    this.height
+                );
+                ctx.fill();
+        
+                ctx.restore();
+        */
+
+        // Apos a criação da função de poligono
         ctx.beginPath();
-        ctx.rect(
-            -this.width/2,
-            -this.height/2,
-            this.width,
-            this.height
-        );
+        ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+        for (let i = 1; i < this.polygon.length; i++) {
+            ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
+        }
         ctx.fill();
-
-        ctx.restore();
 
         this.sensor.draw(ctx);
     }
--- sensor.js
+++ sensor.js
@@ -7,16 +7,51 @@ class Sensor{
         this.raySpread=Math.PI/2;
 
         this.rays=[];
+        this.readings=[];
     }
 
-    update(){
+    update(roadBorders){
+        this.#castRays();
+        this.readings=[];
+        for(let i=0;i<this.rays.length;i++){
+            this.readings.push(
+                this.#getReading(this.rays[i],roadBorders)
+            );
+        }
+    }
+
+    #getReading(ray,roadBorders){
+        let touches=[];
+
+        for(let i=0;i<roadBorders.length;i++){
+            const touch = getIntersection(
+                ray[0],
+                ray[1],
+                roadBorders[i][0],
+                roadBorders[i][1]
+            );
+            if(touch){
+                touches.push(touch);
+            }
+        }
+
+        if(touches.length==0){
+            return null;
+        }else{
+            const offsets=touches.map(e=>e.offset);     // <= Pesquisar sobre
+            const minOffset=Math.min(...offsets);       // <= Pesquisar sobre
+            return touches.find(e=>e.offset==minOffset);
+        }
+    }
+
+    #castRays(){
         this.rays=[];
         for(let i=0;i<this.rayCount;i++){
             const rayAngle=lerp(
                 this.raySpread/2,
                 -this.raySpread/2,
-                i/(this.rayCount-1)
-            );
+                this.rayCount==1?0.5:i/(this.rayCount-1)
+            )+this.car.angle;
             
             const start={x:this.car.x, y:this.car.y};
             const end={
@@ -29,19 +64,37 @@ class Sensor{
         }
     }
 
+
     draw(ctx){
         for(let i=0;i<this.rayCount;i++){
+            let end = this.rays[i][1];
+            if(this.readings[i]){
+                end = this.readings[i];
+            }
+
             ctx.beginPath();
             ctx.lineWidth=2;
-            ctx.strokeStyle="black";
-            console.log(this.rays[i][0]);
-            ctx.moveTo(
+            ctx.strokeStyle="yellow";
+            ctx.moveTo(                 // Inicio da linha do raio
                 this.rays[i][0].x,
                 this.rays[i][0].y
             );
-            ctx.lineTo(
+            ctx.lineTo(                 // Fim da linha do raio
+                end.x,
+                end.y
+            );
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.lineWidth=2;
+            ctx.strokeStyle="black";
+            ctx.moveTo(                 // Inicio da linha do raio
                 this.rays[i][1].x,
                 this.rays[i][1].y
+            );
+            ctx.lineTo(                 // Fim da linha do raio
+                end.x,
+                end.y
             );
             ctx.stroke();
         }
