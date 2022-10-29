0e32cb52aa9763f6ddb3437ebb0102ee3f4e6ab8
erikeugenio <erik.eugenio@outlook.com>
2022-10-29 00:38:35 -0300

    commit

--- car.js
+++ car.js
@@ -12,28 +12,46 @@ class Car {
         this.angle = 0;
         this.damaged = false;
 
+        this.useBrain = controlType == "AI";
+
+
         if (controlType != "DUMMY") {
             this.sensor = new Sensor(this);
+            this.brain = new NeuralNetwork(
+                [this.sensor.rayCount, 6, 4]
+            );
         }
 
         this.controls = new Controls(controlType);
-        //test
+
     }
 
-    update(roadBoarders, traffic) {
+    update(roadBorders, traffic) {
         if (!this.damaged) {
             this.#move();
             this.polygon = this.#createPolygon();
-            this.damaged = this.#assessDamage(roadBoarders, traffic);
+            this.damaged = this.#assessDamage(roadBorders, traffic);
         }
         if (this.sensor) {
-            this.sensor.update(roadBoarders, traffic);
+            this.sensor.update(roadBorders, traffic);
+            const offsets = this.sensor.readings.map(
+                s => s == null ? 0 : 1 - s.offset
+            );
+            const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+            //console.log(outputs);
+
+            if(this.useBrain){
+                this.controls.forward = outputs[0];
+                this.controls.left = outputs[1];
+                this.controls.right = outputs[2];
+                this.controls.reverse = outputs[3];
+            }
         }
     }
 
-    #assessDamage(roadBoarders, traffic) {
-        for (let i = 0; i < roadBoarders.length; i++) {
-            if (polysIntersect(this.polygon, roadBoarders[i])) {
+    #assessDamage(roadBorders, traffic) {
+        for (let i = 0; i < roadBorders.length; i++) {
+            if (polysIntersect(this.polygon, roadBorders[i])) {
                 return true;
             }
         }
@@ -42,6 +60,7 @@ class Car {
                 return true;
             }
         }
+        return false;
     }
 
     #createPolygon() {
@@ -106,7 +125,7 @@ class Car {
         this.y -= Math.cos(this.angle) * this.speed;
     }
 
-    draw(ctx, color) {
+    draw(ctx, color, drawSensor = false) {
 
         if (this.damaged) {
             ctx.fillStyle = "gray";
@@ -141,7 +160,7 @@ class Car {
         }
         ctx.fill();
 
-        if (this.sensor) {
+        if (this.sensor && drawSensor) {
             this.sensor.draw(ctx);
         }
 
